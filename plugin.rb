9a26b31ae180de8b2336cb5bb690ca4df74fc02c
Bannon Tanner <bannon.n.tanner@gmail.com>
2025-08-28 23:00:43 -0500

    name refactor and remove logger

--- plugin.rb
+++ plugin.rb
@@ -5,7 +5,7 @@
 # meta_topic_id: TODO
 # version: 0.0.1
 # authors: Discourse
-# url: TODO
+# url: https://github.com/tannerabread/zero-click-sso-plugin
 # required_version: 2.7.0
 
 enabled_site_setting :zero_click_sso_enabled
@@ -21,37 +21,28 @@ after_initialize do
   Rails.logger.info("Zero Click SSO plugin initialized")
 
   module ::ZeroClickSso
-    Logger = ActiveSupport::Logger.new(Rails.root.join("log", "zero_click_sso.log"))
-
     module ControllerExtension
       def self.prepended(base)
-        base.before_action :zero_click_log_every_request
+        base.before_action :zero_click_login
       end
 
       private
 
-      def zero_click_log_every_request
-        ZeroClickSso::Logger.warn(
-          "Zero Click SSO enabled=#{SiteSetting.zero_click_sso_enabled} " \
-            "local_logins=#{SiteSetting.enable_local_logins} " \
-            "path=#{request.path} format=#{request.format} xhr=#{request.xhr?}",
-        )
-
+      def zero_click_login
         return unless SiteSetting.zero_click_sso_enabled
         return if SiteSetting.enable_local_logins
         return unless request.format.html? && !request.xhr?
+        # Detect if there is an active forum session
         return if current_user.present?
 
         # prevent loop with omniauth endpoints
         return if request.path.start_with?("/auth/")
 
         auths = Discourse.enabled_authenticators
-        ZeroClickSso::Logger.warn("auths size = #{auths.size}")
         return unless auths.size == 1
 
         provider = auths.first.name
 
-        ZeroClickSso::Logger.warn("Current home of SSO probe, provider = #{provider}")
         redirect_to "/auth/#{provider}?prompt=none&origin=#{CGI.escape(request.original_fullpath)}"
       end
     end
