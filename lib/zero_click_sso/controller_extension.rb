d9ac121cf486d5975d2d4142ac97306b945c70a5
Bannon Tanner <bannon.n.tanner@gmail.com>
2025-09-26 00:03:07 -0500

    extension cleanup

--- lib/zero_click_sso/controller_extension.rb
+++ lib/zero_click_sso/controller_extension.rb
@@ -10,39 +10,42 @@ module ::ZeroClickSso
     private
 
     def zero_click_login
-      return unless SiteSetting.zero_click_sso_enabled
-      return if SiteSetting.enable_local_logins || SiteSetting.enable_local_logins_via_email
+      return unless should_attempt_zero_click?
 
-      # Only first page load HTML requests
-      return unless request.format.html? && !request.xhr?
-      return if request.path.end_with?(".webmanifest")
-
-      # Detect if there is an active forum session
-      return if current_user.present?
-
-      # prevent loop with omniauth endpoints
-      return if request.path.start_with?("/auth/")
-
-      return if session[:zero_click_attempted] || session[:zero_click_failed]
-
-      auths = Discourse.enabled_authenticators
-      return unless auths.size == 1
-
-      provider = auths.first.name
-      return if provider.blank?
+      auth = Discourse.enabled_authenticators.first
+      return unless auth
+      return if auth.name.blank?
 
       session[:zero_click_attempted] = true
       session[:zero_click_origin] = request.original_fullpath
 
-      qs = Rack::Utils.build_query(prompt: "none", origin: request.original_fullpath)
-      redirect_to path("/auth/#{provider}?#{qs}")
+      redirect_to path("/auth/#{auth.name}?#{build_silent_query}")
+    end
+
+    def should_attempt_zero_click?
+      SiteSetting.zero_click_sso_enabled &&
+        !SiteSetting.enable_local_logins &&
+        !SiteSetting.enable_local_logins_via_email &&
+        request.format.html? &&
+        !request.xhr? &&
+        !request.path.end_with?(".webmanifest") &&
+        current_user.blank? &&
+        !request.path.start_with?("/auth/") &&
+        !session[:zero_click_attempted] &&
+        !session[:zero_click_failed] &&
+        Discourse.enabled_authenticators.size == 1
+    end
+
+    def build_silent_query
+      Rack::Utils.build_query(prompt: "none", origin: request.original_fullpath)
     end
 
     def cleanup_zero_click_session
-      if session[:zero_click_failed] && !request.path.start_with?("/auth/")
-        session.delete(:zero_click_attempted)
-        session.delete(:zero_click_failed)
-      end
+      return unless session[:zero_click_failed]
+      return if request.path.start_with?("/auth/")
+        
+      session.delete(:zero_click_attempted)
+      session.delete(:zero_click_failed)
     end
   end
 end
