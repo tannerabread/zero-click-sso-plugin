be04e66a9584d1d6b043c7cd52087dee2016179c
Bannon Tanner <bannon.n.tanner@gmail.com>
2025-09-25 23:51:55 -0500

    add new tests

--- spec/requests/zero_click_sso_spec.rb
+++ spec/requests/zero_click_sso_spec.rb
@@ -1,20 +1,16 @@
 # frozen_string_literal: true
 
-RSpec.configure do |config|
-  config.before(:suite) do
-    SiteSetting.enable_local_logins_via_email = false
-    SiteSetting.enable_local_logins = false
-    SiteSetting.enable_passkeys = false
-    SiteSetting.zero_click_sso_enabled = true
-  end
-end
+require "rails_helper"
 
-RSpec.describe "ZeroClickSso Plugin" do
+RSpec.describe "ZeroClickSso Plugin", type: :request do
   before do
     SiteSetting.zero_click_sso_enabled = true
     SiteSetting.enable_local_logins = false
+    SiteSetting.enable_local_logins_via_email = false
     SiteSetting.enable_google_oauth2_logins = false
     SiteSetting.enable_github_logins = false
+
+    reset_session if respond_to?(:reset_session)
   end
   it "is enabled site setting" do
     expect(SiteSetting.zero_click_sso_enabled).to be true
@@ -30,6 +26,14 @@ RSpec.describe "ZeroClickSso Plugin" do
 
     it "does nothing when local logins enabled" do
       SiteSetting.enable_local_logins = true
+      get "/"
+      expect(response).to be_successful
+      expect(response).not_to redirect_to(/auth/)
+    end
+
+    it "does nothing when local email logins enabled" do
+      SiteSetting.enable_local_logins = true
+      SiteSetting.enable_local_logins_via_email = true
       get "/"
       expect(response).to be_successful
       expect(response).not_to redirect_to(/auth/)
@@ -53,26 +57,61 @@ RSpec.describe "ZeroClickSso Plugin" do
       get "/auth/google_oauth2/callback"
       expect(response).not_to redirect_to(/auth/)
     end
+
+    it "does nothing if multiple providers are configured" do
+      SiteSetting.enable_google_oauth2_logins = true
+      SiteSetting.enable_github_logins = true
+
+      get "/"
+      expect(response).to be_successful
+      expect(response).not_to redirect_to(/auth/)
+    end
   end
 
-  it "redirects to the provider with prompt=none" do
-    SiteSetting.enable_google_oauth2_logins = true
-    provider = Discourse.enabled_authenticators.first.name
+  describe "zero click login attempt" do
+    it "redirects to the provider with prompt=none" do
+      SiteSetting.enable_google_oauth2_logins = true
+      provider = Discourse.enabled_authenticators.first.name
 
-    get "/"
-    expect(response).to redirect_to(%r{auth/#{provider}\?prompt=none&origin=%2F})
+      get "/"
+      expect(response).to redirect_to(%r{auth/#{provider}\?prompt=none&origin=%2F})
+    end
+
+    it "does not attempt SSO twice in same session" do
+      SiteSetting.enable_google_oauth2_logins = true
+      get "/"
+      expect(response).to redirect_to(/auth/)
+      
+      get "/"
+      expect(response).to be_successful
+      expect(response).not_to redirect_to(/auth/)
+    end
+  end
+end
+
+RSpec.describe ZeroClickSso::FailureHandler do
+  def call_handler(path, rack_session: {}, extra_env: {})
+    env = Rack::MockRequest.env_for(path, "rack.session" => rack_session).merge(extra_env)
+    status, headers, body = described_class.call(env)
+    [status, headers, body, env["rack.session"]]
   end
 
-  it "does nothing if multiple providers are configured" do
-    SiteSetting.enable_google_oauth2_logins = true
-    SiteSetting.enable_github_logins = true
+  it "redirects back to origin on silent login_required" do
+    status, headers, _body, session =
+      call_handler("/auth/failure?prompt=none&error=immediate_failed",
+                   rack_session: { zero_click_origin: "/t/123" })
 
-    get "/"
-    expect(response).to be_successful
-    expect(response).not_to redirect_to(/auth/)
+    expect(status).to eq(302)
+    expect(headers["location"]).to eq("/t/123")
+    expect(session[:zero_click_failed]).to be true
+    expect(session[:zero_click_origin]).to be_nil
   end
 
-  # TODO: Test the /auth/failure endpoint end to end
-  # Request specs are unreliable here due to OmniAuth + bypass middleware intercepting /auth/*
-  #   in the test stack
+  it "falls back to default failure handler if not silent" do
+    status, headers, _body, _session =
+      call_handler("/auth/failure?error=invalid_credentials")
+
+    expect(status).to eq(302)
+    expect(headers["location"]).to match(%r{/auth/failure\?message=})
+  end
 end
